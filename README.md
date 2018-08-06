1e88ab53b8c5b7c74b5d7f91b2e4b964750453b0
yaleman <yaleman@ricetek.net>
2018-08-06 14:54:05 +1000

    environment variables for forwarders, cleaner

--- README.md
+++ README.md
@@ -26,12 +26,28 @@ You'll need to enable the `response-policy` option in BIND. This requires versio
 
 Make sure you add the blacklist zone file to your config. For example, I added `include "/etc/bind/named.conf.ky-rpz";` to `/etc/bind/named.conf.local` on a Debian machine.
 
+# Docker Image
+
+## Building the image (necessary if you want custom options or upstream forwarders 
+
+Set your configuration variables in `conf/env.env` (an example is in `conf/env.env.example`) and run `build.sh`.
+
+## Setting custom forwarders
+
+Put an entry in `conf/env.env`
+
+
+## Running it
+
+To spin up a new instance: `docker run --name "ky-rpz" -p 53:53/udp -p 53:53 --rm yaleman/ky-rpz`
+
+To force an update of the definitions (should happen hourly in cron): `docker exec -it ky-rpz /opt/ky-rpz/bin/update.sh`
+
 ## Squid blocking
 
 Below is a section of the default configuration file from Ubuntu's squid package (`/etc/squid/squid.conf`). We've added the kyrpz block to the top, just below the "INSERT YOUR OWN RULES" section. Do this yourself, and it'll do the needful.
 
 ```
-
 #
 # INSERT YOUR OWN RULE(S) HERE TO ALLOW ACCESS FROM YOUR CLIENTS
 #
@@ -46,18 +62,6 @@ http_access allow localhost
 # And finally deny all other access to this proxy
 http_access deny all
 ```
-
-# Docker Image
-
-## Building the image (necessary if you want custom options or upstream forwarders 
-
-Set your configuration variables in conf/ky-rpz.config.docker and run `build.sh`.
-
-## Running it
-
-To spin up a new instance: `docker run --name "ky-rpz" -p 53:53/udp -p 53:53 --rm yaleman/ky-rpz`
-
-To force an update of the definitions (should happen hourly in cron): `docker exec -it ky-rpz /opt/ky-rpz/bin/update.sh`
 
 # TODO
 
--- bin/ky-rpz.sh
+++ bin/ky-rpz.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f "conf/ky-rpz.config" ]; then
         echo "Couldn't find config file (ky-rpz.config)"
-        exit 
+        exit
 else
         source "./conf/ky-rpz.config"
 fi
@@ -23,9 +23,9 @@ if [ ! -d "$ZONEFILEDIR" ]; then
 fi
 
 if [ ! -z $FORWARDERS ]; then
-        echo "[+] updating forwarder config"
+        echo "[+] updating forwarder config, using $FORWARDERS"
         sed  "s@//#FORWARDERS#@forwarders { $FORWARDERS; };@" $INSTALLDIR/conf/named.conf.options.example | sed 's/;;/;/g' > $INSTALLDIR/conf/named.conf.options
-else   
+else
         echo "[+] no forwarders specified, using root hints"
 fi
 
@@ -54,19 +54,25 @@ echo "[+] Getting list of domains to blacklist"
 
 #TODO: parse this: https://urlhaus.abuse.ch/downloads/rpz/
 
+# Domain Lists
 wget -nv -O "$TEMPDIR/yoyo.list" "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=nohtml&showintro=0&mimetype=plaintext"
-
 wget -nv -O "$TEMPDIR/mal-list.list" 'http://mirror1.malwaredomains.com/files/justdomains'
-
 wget -nv -O "$TEMPDIR/abusech-ransomware-list.list" 'http://ransomwaretracker.abuse.ch/downloads/RW_DOMBL.txt'
 wget -nv -O "$TEMPDIR/abusech-zeus-list.list" 'https://zeustracker.abuse.ch/blocklist.php?download=baddomains'
-
 wget -nv -O "$TEMPDIR/webminer.list" 'https://raw.githubusercontent.com/greatis/Anti-WebMiner/master/blacklist.txt'
+
+# URL lists
+
+wget -nv -O "$TEMPDIR/cybercrime-tracker.urls" "https://cybercrime-tracker.net/all.php"
+
+echo "[+] Cleaning up URL lists"
+# filter out www.* (proto):// and get rid of port/uri entries
+sed -e 's/^www\.//' -e 's#^(http|https|ftp)\://##' $TEMPDIR/*.urls | awk -F"/" '{print $1}' | awk -F":" '{print $1}'  > "$TEMPDIR/urls.list"
 
 # compile into single unique list
 echo "[+] Cleaning up domains"
 # strip empty lines and commented lines from the lists
-sed -e '/^$/d' -e '/^\[/d' -e '/^\#/d' -e 's/[^\s]*\s//' -i $TEMPDIR/*.list
+sed -e '/^$/d' -e '/^\[/d' -e '/^\#/d' -e 's/[^\s]*\s//' -e 's/^www\.//' -e 's/\:[0-9]+$//' -i $TEMPDIR/*.list
 # grab a unique, sorted list of all the .list files' contents
 sort -u $TEMPDIR/*.list > $TEMPDIR/$DBFILE
 
